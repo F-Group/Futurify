b73998a7dadf2dff4ad2c8f738b38b2b21c5c06f
Duy Tan <duytan1909@gmail.com>
2016-07-26 23:09:52 +0700

    add blog list page by tag

---
+++ wp-content/themes/futurity/tag.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<div class="bl-blog-list">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="bl-blog-list-title">
+                    <h1>OUR CULTURE</h1>
+                    <p>A place we share to each other, care of things around us.
+                        It's our pleasure to share culture, thoughts, interests in technology with you - our clients as well as the new visitors</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="bl-blog-list-content ">
+                    <div class="row list-blog">
+                        <?php $tag = get_query_var("tag") ?>
+                        <?php echo do_shortcode("[ajax_load_more post_type='post' posts_per_page='6' max_pages='3' container_type='div' css_classes='col-xs-12 col-sm-12 col-md-12' button_loading_label='' tag='{$tag}']") ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
--- wp-content/themes/futurity/templates/page-blog-list-template.php
+++ wp-content/themes/futurity/templates/page-blog-list-template.php
@@ -70,10 +70,7 @@ $google_map = get_field('location');
                         </div>
                     </div>
                     <div class="row list-blog">
-                        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                            <?php the_content(); ?>
-                        <?php endwhile; ?>
-                        <?php endif; ?>
+                        <?php echo do_shortcode("[ajax_load_more post_type='post' posts_per_page='6' max_pages='3' container_type='div' css_classes='col-xs-12 col-sm-12 col-md-12' button_loading_label='']") ?>
                     </div>
                 </div>
             </div>
