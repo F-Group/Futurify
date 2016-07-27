9d41de5c1b92960cbf29c81f95cc2b5cfa5937b4
Duy Tan <duytan1909@gmail.com>
2016-07-27 23:16:15 +0700

    update home page

--- wp-content/themes/futurity/index.php
+++ wp-content/themes/futurity/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
-<div id="carousel-example-vertical" class="carousel vertical slide">
+<section id="carousel-example-vertical" class="section carousel vertical slide">
     <div class="carousel-inner" role="listbox">
         <div class="item active">
-            <img src="<?php bloginfo('template_url') ?>/img/menu.png">
+            <video src="<?php bloginfo('template_url') ?>/demo_video.mp4" autoplay></video>
             <div class="caption">
                 <h2>OUR MISSION</h2>
                 <p>We offer remote world-class engineers and build reliable, scalable and measurable solutions to help you grow and succeed</p>
@@ -27,24 +27,36 @@
         <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
-</div>
-<div class="block">
-    <!-- <section class="section banner bg-img" style="background: url('<?php echo get_background(); ?>')">
-        <div class="mask fp-table">
-            <div class="fp-tableCell">
-                <div class="content container">
-                    <h1 class="title"></h1>
-                    <div class="sub-title">
-                        <p class="line"></p>
-                        <p class="text"></p>
-                        <p class="line"></p>
-                    </div>
+</section>
+<section class="home-introduction">
+    <div class="container">
+        <div class="col-sm-3">
+            <div content="row">
+                <h1>WE'RE EXCITED TO LEARN MORE ABOUT YOU AND YOUR NEEDS</h1>
+            </div>
+        </div>
+        <div class="col-sm-5">
+            <div class="row introduction-img">
+                <img class="img-responsive" src="<?php bloginfo('template_url') ?>/img/content/home/home-introdution-img.png">
+            </div>
+        </div>
+        <div class="col-sm-4 home-panel">
+            <div class="row introduction-panel">
+                <img class="panel-frame img-responsive" src="<?php bloginfo('template_url') ?>/img/content/home/white-line-background.png">
+                <div class="panel-chat vertical-center-Y">
+                    <form class="panel-content">
+                        <h3>Let's chat and create great works together!!!</h3>
+                        <p>Hello, my name is</p>
+                        <input type="text" class="input-style">
+                        <p>Reach me at this email</p>
+                        <input type="email" class="input-style">
+                        <p>And have conversation of my concern about</p>
+                        <input type="text" class="input-style">
+                        <div class="form-btn"><button class="btn-play1"></button></div>
+                    </form>
                 </div>
             </div>
         </div>
-    </section> -->
-    <section class="section">
-
-    </section>
-</div>
+    </div>
+</section>
 <?php get_footer(); ?>
