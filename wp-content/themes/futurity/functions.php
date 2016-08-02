702d9fa4c547ae37e31d75bad2b5e333e83e5c40
DungTN <dungtran@goappable.com>
2016-08-02 22:11:19 +0700

    fix  Khi đang ở page blog post detail thì phải highlight trên navigation

--- wp-content/themes/futurity/functions.php
+++ wp-content/themes/futurity/functions.php
@@ -140,3 +140,29 @@ function getList($type,$order_by="date") {
         'order'=>'DESC');
     return new WP_Query($args);
 }
+
+
+function remove_parent_classes($class)
+{
+    // check for current page classes, return false if they exist.
+    return ($class == 'current_page_item' || $class == 'current_page_parent' || $class == 'current_page_ancestor'  || $class == 'current-menu-item') ? FALSE : TRUE;
+}
+
+function add_class_to_wp_nav_menu($classes)
+{
+    switch (get_post_type())
+    {
+        case 'post':
+            // we're viewing a custom post type, so remove the 'current_page_xxx and current-menu-item' from all menu items.
+            $classes = array_filter($classes, "remove_parent_classes");
+
+            // add the current page class to a specific menu item (replace ###).
+            if (in_array('menu-item-20', $classes))
+            {
+                $classes[] = 'current-menu-item';
+            }
+            break;
+    }
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'add_class_to_wp_nav_menu');
