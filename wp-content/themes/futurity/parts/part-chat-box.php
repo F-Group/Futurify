6b538ffabb55bff57ab0ad9b530fd39bcb36f480
DungTN <dungtran@goappable.com>
2016-08-06 23:45:21 +0700

    implement chat box

--- wp-content/themes/futurity/parts/part-chat-box.php
+++ wp-content/themes/futurity/parts/part-chat-box.php
@@ -1,7 +1,5 @@
-
-<div class="chat-box clearfix hidden-xs">
-    <a href="">
-        <div class="chat-box-title row items-group align-row vertical-center">
+<div class="chat-box clearfix hidden-xs" xmlns="http://www.w3.org/1999/html">
+    <a href="" class="chat-box-title items-group align-row vertical-center">
             <div class="col-md-2 chat-title-icon items-group vertical-center">
                 <i class="chat-icon"></i>
             </div>
@@ -9,6 +7,11 @@
                 <span class="title">We’re online!</span>
                 <i class="arrow arrow-up"></i>
             </div>
-        </div>
     </a>
+<!--    <div class="chat-box-content">-->
+<!--    </div>-->
+<!--    <div class="chat-box-input items-group align-row vertical-center">-->
+<!--        <input placeholder="Type something.." type="text"> </input>-->
+<!--        <i class="send"></i>-->
+<!--    </div>-->
 </div>
