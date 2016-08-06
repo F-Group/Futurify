483882b08c29e7ce7c80e41426838e32bdff6e92
DungTN <dungtran@goappable.com>
2016-08-07 01:21:04 +0700

    implement chat box

--- wp-content/themes/futurity/parts/part-chat-box.php
+++ wp-content/themes/futurity/parts/part-chat-box.php
@@ -1,17 +1,34 @@
 <div class="chat-box clearfix hidden-xs" xmlns="http://www.w3.org/1999/html">
-    <a href="" class="chat-box-title items-group align-row vertical-center">
-            <div class="col-md-2 chat-title-icon items-group vertical-center">
-                <i class="chat-icon"></i>
-            </div>
-            <div class="col-md-10 chat-title-content items-group align-row vertical-center">
-                <span class="title">We’re online!</span>
-                <i class="arrow arrow-up"></i>
-            </div>
+    <a href="" class="chat-box-title items-group align-row vertical-center" data-toggle="dropdown"
+       aria-expanded="false">
+        <div class="col-md-2 chat-title-icon items-group vertical-center">
+            <i class="chat-icon"></i>
+        </div>
+        <div class="col-md-10 chat-title-content items-group align-row vertical-center">
+            <span class="title">We’re online!</span>
+            <i class="arrow arrow-up"></i>
+            <i class="arrow arrow-down"></i>
+        </div>
     </a>
-<!--    <div class="chat-box-content">-->
-<!--    </div>-->
-<!--    <div class="chat-box-input items-group align-row vertical-center">-->
-<!--        <input placeholder="Type something.." type="text"> </input>-->
-<!--        <i class="send"></i>-->
-<!--    </div>-->
+    <div class="dropdown-menu">
+        <div class="chat-box-content">
+            <div class="row">
+                <span class="pull-left col-md-8 message client">
+                    How can we help you?
+                </span>
+            </div>
+            <div class="row">
+                <span class="col-md-8 pull-right message own">
+                     Lorem ipsum dolor<br> sit amet, consectetuer.
+                </span></div>
+        </div>
+        <div class="chat-box-input items-group align-row vertical-center">
+            <input placeholder="Type something.." type="text"> </input>
+            <span class="fa-stack fa-lg send">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-paper-plane-o fa-stack-1x fa-inverse"></i>
+            </span>
+        </div>
+    </div>
+
 </div>
